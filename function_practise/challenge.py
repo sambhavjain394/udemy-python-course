7e2a98429400ea36d89d3a088672d19ee4af6670
ssaahhaajj <ssaahhaajj@gmail.com>
2020-10-08 23:34:59 +0530

    Improving Code Logic

--- function_practise/challenge.py
+++ function_practise/challenge.py
@@ -5,10 +5,10 @@
 def spy_game(lis):
     temp=[]
     for item in lis:
-        if item==0 or item==7:
+        if item==0 or item==5:
             temp.append(item)
             
-    for i in range(0,len(temp)-2):
+    for i in range(0,len(temp)-1):
         if (temp[i]*100 +temp[i+1]*10+temp[i+2])==7:
             return True
     return False
@@ -25,7 +25,7 @@ def count_primes(num):
         if visited[i]==True:
             for j in range(i*i,1000,i):
                 visited[j]=False
-    count=0
+    count=1
     for i in range(0,num+1):
         if visited[i]==True:
             count+=1
